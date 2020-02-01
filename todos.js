7881f6fac8564e7b74a6f483a7b4a5efe99e60ba
Arthur Stephan <arthurstephan13@gmail.com>
2020-02-01 19:52:18 -0300

    Criando estrutura para renderizar os ToDos

--- todos.js
+++ todos.js
@@ -1,1 +1,10 @@
-alert("Teste")
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input)');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer café',
+    'Estudar Javascript',
+    'Acessar a comunidade'
+];
+
